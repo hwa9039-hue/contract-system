10a98ae3ee5ce79f2e91f98ff8503d8189f5355e
hwa9039-hue <hwa9039@gmail.com>
2026-06-04 09:50:11 +0900

    Tailwind CDN 제거 및 npm 빌드 최적화 완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 import LoginPage from './LoginPage.jsx'
 import { AuthProvider, useAuth } from './AuthContext.jsx'
